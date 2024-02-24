a708b46589428b73d00ad68627e5c58b36fec91c
MAGIC Reincarnated <139699919+MagicReincarnation@users.noreply.github.com>
2024-02-24 17:45:05 +0700

    jslisensihiru2.js
    warning lisensi

---
+++ jslisensihiru2.js
@@ -0,0 +1,34 @@
+function wrqardyfufuvjgwdyfibivjvtctdufdheeyf7guruefh() {
+    document.body.classList.add("nqbaoshdneoshxbdsmdnxn");
+    const qidonwjwjxgiwnsgxgahaokqheme = document.createElement("div");
+    qidonwjwjxgiwnsgxgahaokqheme.innerHTML = `<style>
+        .nqbaoshdneoshxbdsmdnxn{overflow:hidden;}
+        #pwjnwzywbwjsbxysywbs span{font-size:50px}#pwjnwzywbwjsbxysywbs{position: fixed!important;z-index: 999999!important;top: 0!important;left: 0!important;width: 100%!important;height: 100%!important;display: flex!important;
+        background-color: rgba(0,0,0, 0.6)!important;-webkit-backdrop-filter: blur(5px);backdrop-filter: blur(5px);}
+        #pwjnwzywbwjsbxysywbs-dwxce{display: block!important;margin: auto!important;width: 100%!important;max-width: 80%!important;height:100% !important;max-height:80%!important;text-align: center!important;background: #fff!important;color:#222;padding: 40px!important;border-radius: 8px!important;}
+        #pwjnwzywbwjsbxysywbs h4{font-size:20px}
+        #sjswkshxbdnwoqownnvv{color:#f44336;}
+        #Hnssidfrfufftfsfhfufjffai_pwjnwzywbwjsbxysywbs {color:#ff950d;}
+        </style><div class='nqbaoshdneoshxbdsmdnxnb' id="pwjnwzywbwjsbxysywbs">
+        <div id="pwjnwzywbwjsbxysywbs-dwxce"><h4>PERINGATAN!</h4>
+        <p>domain website <b id='Hnssidfrfufftfsfhfufjffai_pwjnwzywbwjsbxysywbs'></b> milik Kamu belum aktif</p> <p>Untuk mengaktifkannya</p>
+        <p>Silahkan hubungi Hirutshuji diLink ini "<a id="sjswkshxbdnwoqownnvv" href='https://datakodehiru.blogspot.com/p/lisensi-user.html'><b id='webHnssidfrfufftfsfhfufjffai_pwjnwzywbwjsbxysywbs'>Hirutshuji</b></a>"  untuk mendapatkan lisensi Template</p>
+        <span id="sjswkswhyqgabhxbdnwoqownnvv"></span></div></div>`;
+    document.body.appendChild(qidonwjwjxgiwnsgxgahaokqheme);
+    let qoqnsbxbsnxnxns = e7k4d5c9r1p8tH;
+    const Hnssidfrfufftfsfhfufjffai_pwjnwzywbwjsbxysywbsWeb = document.getElementById('Hnssidfrfufftfsfhfufjffai_pwjnwzywbwjsbxysywbs');
+ Hnssidfrfufftfsfhfufjffai_pwjnwzywbwjsbxysywbsWeb.textContent = qoqnsbxbsnxnxns;
+    const aswonsksjstivajsjssiShsuwpan = document.getElementById('sjswkswhyqgabhxbdnwoqownnvv');
+    let asssswonsksjstivajsjssiShehehwhsuwpan = x456y7sz;     
+    aswonsksjstivajsjssiShsuwpan.textContent = asssswonsksjstivajsjssiShehehwhsuwpan;
+    
+    const isanaaterssvsssalsfvbIgvdbb = setInterval(function () {
+        asssswonsksjstivajsjssiShehehwhsuwpan--;
+        if (asssswonsksjstivajsjssiShehehwhsuwpan === 0) {
+            clearInterval(isanaaterssvsssalsfvbIgvdbb);             
+  svscadlidnafjadLsaicenfffse = false;
+  window.location.href = "https://datakodehiru.blogspot.com/"; 
+        }
+        aswonsksjstivajsjssiShsuwpan.textContent = asssswonsksjstivajsjssiShehehwhsuwpan;
+    }, 1000);
+}
